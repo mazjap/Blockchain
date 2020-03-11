e60ef09be79914a261c7b4b7203c6521edc11a9c
Jordan Christensen <jordan.c4922@gmail.com>
2020-03-11 16:10:05 -0600

    Implement local blockchain mining, and server verification

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -1,2 +1,93 @@
-# Paste your version of blockchain.py from the basic_block_gp
-# folder here
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block)
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def hash(self, block):
+        string_block = json.dumps(block, sort_keys=True)
+        raw_hash = hashlib.sha256(string_block.encode())
+        hex_hash = raw_hash.hexdigest()
+        return hex_hash
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def valid_proof(block_string, proof):
+        guess = f'{block_string}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:6] == '000000'
+
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    data = request.get_json()
+
+    if data:
+        user_proof = data['proof']
+        user_id = data['id']
+
+    if user_id is None or user_proof is None:
+        return jsonify({'message': 'Not a valid proof of work'}), 400
+    
+    block_string = json.dumps(blockchain.last_block, sort_keys=True)
+    if blockchain.valid_proof(block_string, user_proof):
+        block = blockchain.new_block(user_proof)
+        code = 200
+        response = {
+            'message': 'New block forged',
+            'forged_block': block
+        }
+    else:
+        code = 400
+        response = {
+            'message': 'Not a valid proof of work'
+        }
+    return jsonify(response), code
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+@app.route('/last_block', methods=['GET'])
+def last_block():
+    response = {
+        'last_block': blockchain.last_block
+    }
+    return jsonify(response), 200
+
+# Run the program on port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -3,7 +3,7 @@ import requests
 
 import sys
 import json
-
+import time
 
 def proof_of_work(block):
     """
@@ -13,7 +13,14 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        if proof % 10_000_000 is 0 and get_latest_block() == block:
+            return -1
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,8 +34,20 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
 
+    return guess_hash[:6] == '000000'
+
+def get_latest_block():
+    r = requests.get(url=node + "/last_block")
+    try:
+        data = r.json()
+        return data
+    except ValueError:
+        print("Error:  Non-json response")
+        print("Response returned:")
+        print(r)
 
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
@@ -36,6 +55,8 @@ if __name__ == '__main__':
         node = sys.argv[1]
     else:
         node = "http://localhost:5000"
+
+    coin_balance = 0
 
     # Load ID
     f = open("my_id.txt", "r")
@@ -45,26 +66,31 @@ if __name__ == '__main__':
 
     # Run forever until interrupted
     while True:
-        r = requests.get(url=node + "/last_block")
-        # Handle non-json response
-        try:
-            data = r.json()
-        except ValueError:
-            print("Error:  Non-json response")
-            print("Response returned:")
-            print(r)
-            break
+        data = get_latest_block()
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        start_time = time.time()
+        new_proof = proof_of_work(data['last_block'])
+        total_time = time.time() - start_time
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
+        try:
+            data = r.json()
+        except ValueError:
+            print("Error decoding value")
+            print(r)
 
+        if data['message'] == 'New block forged':
+            coin_balance += 1
+            print(f"A new block has been created in {total_time} seconds and you have received a coin.")
+            print(f"Total balance: {coin_balance}")
+        else:
+            print('HOLY FUCK NO NO NO SHIT OH FUCK')
+            print(data['message'])
         # TODO: If the server responds with a 'message' 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
+        # add 1 to the number of coins mined and print it. Otherwise,
         # print the message from the server.
-        pass
+        
